02e5a7746113f2e69c9e36a76b0a3146bb4bf28d
nikhilnagargoje <nikhilnagargoje8502@gmail.com>
2025-11-29 11:36:19 +0530

    Use environment variable for server port

--- backend/server.js
+++ backend/server.js
@@ -82,4 +82,5 @@ app.get("/test-email", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("✔ Server running on port 5000"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("✔ Server running on port " + port));
